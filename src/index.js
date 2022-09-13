aa5c8d6c2bd0b1a5b2b26c0d5fa8a3f4334a75ee
Joshua <cutejosh2@gmail.com>
2022-09-13 06:07:33 +0100

    first component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
+function BookList() {
+	return (<div>
+		<h2>This is a Book list</h2>
+	</div>);
+}
+
+
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
+
