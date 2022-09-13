3d0f5c4b69b9a14ddd52d2711e70608ded701bc1
Joshua <cutejosh2@gmail.com>
2022-09-13 06:18:18 +0100

    created reusable components

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function BookList() {
-	return (<div>
-		<h2>This is a Book list</h2>
-	</div>);
+	return (
+		<section>
+			<Book />
+			<Book />
+		</section>
+	);
 }
 
+const Book = () => {
+	return <article>
+		<Title />
+		<Image />
+		<Author />
+	</article>
+}
 
+const Image = () => {
+	return <img src="someimagesource" alt="Image" />
+}
+
+const Author = () => <h5>Author here</h5>;
+const Title = () => <h2>Some TItle</h2>
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
 
