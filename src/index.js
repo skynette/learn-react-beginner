c3e560c9cb7b7a6dab496ddeb0dc1def21c45f97
Joshua <cutejosh2@gmail.com>
2022-09-19 22:50:18 +0100

    refactor props 3

--- src/index.js
+++ src/index.js
@@ -3,25 +3,25 @@ import ReactDOM from "react-dom";
 import './index.css';
 
 
-const author = "Joshua Hassan The First"
-const title = "some title goes here"
-const image = "https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/81abeGoTMsL._AC_UL600_SR600,400_.jpg"
-const image_2 = "https://images-na.ssl-images-amazon.com/images/I/81HE+JDes-L._AC_UL600_SR600,400_.jpg"
-
-const books = {
-	img: "somelink",
-	title: "some titile",
-	author: "author name",
-};
+const books = [
+	{
+		img: "https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/81abeGoTMsL._AC_UL600_SR600,400_.jpg",
+		title: "Title one of books",
+		author: "author name one",
+	},
+	{
+		img: "https://images-na.ssl-images-amazon.com/images/I/81HE+JDes-L._AC_UL600_SR600,400_.jpg",
+		title: "Title two of booke",
+		author: "author name two",
+	},
+]
 
 function BookList() {
 	return (
 		<section className="booklist">
-			<Book author={ author } title={ title } image={ image } />
-			<Book author={ author } title="over written title" image={ image_2 } />
-			<Book author={ author } title="over written title" image={ image_2 }>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum architecto voluptate perferendis! Delectus illum in illo voluptatibus dolorum commodi eius voluptates voluptatum provident non dolorem veritatis, voluptatem ex deserunt amet.</p>
-			</Book>
+			{ books.map(()=>{
+				return "hello"
+			}) }
 		</section>
 	);
 }
@@ -32,7 +32,6 @@ const Book = (props) => {
 		<img src={ props.image } alt="" />
 		<h2>{ props.title }</h2>
 		<h4>{ props.author }</h4>
-		{ props.children }
 	</article>
 }
 
