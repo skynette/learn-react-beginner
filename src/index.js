15580794302bea297402046c268ae1dd95c47ebb
Joshua <cutejosh2@gmail.com>
2022-09-19 21:10:07 +0100

    intro to props

--- src/index.js
+++ src/index.js
@@ -2,28 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 
+
+const author = "Joshua Hassan The First"
+const title = "some title goes here"
+const image = "https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/81abeGoTMsL._AC_UL600_SR600,400_.jpg"
+
 function BookList() {
 	return (
 		<section className="booklist">
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
 			<Book />
 		</section>
 	);
 }
 
 const Book = () => {
-	const title = "some title goes here"
 	return <article className="book">
-		<img src="https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/81abeGoTMsL._AC_UL600_SR600,400_.jpg" alt="best seller picture" />
-		<h4>Joshua Hassan</h4>
+		<img src={image} alt="" />
+		<h4>{author}</h4>
 		<h2>{title}</h2>
-		<h4>Some content yeah</h4>
 	</article>
 }
 
