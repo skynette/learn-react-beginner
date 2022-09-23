f59c1dda3de542bcd4228b81e7335326b89a958a
Joshua <cutejosh2@gmail.com>
2022-09-23 21:35:36 +0100

    added 4th book

--- src/index.js
+++ src/index.js
@@ -21,6 +21,12 @@ const books = [
 		title: "Title three of booke",
 		author: "author name three",
 	},
+	{
+		id: 4,
+		img: "https://images-na.ssl-images-amazon.com/images/I/81HE+JDes-L._AC_UL600_SR600,400_.jpg",
+		title: "Title four of booke",
+		author: "author name four",
+	},
 ]
 
 function BookList() {
