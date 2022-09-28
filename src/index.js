dd9df46645cd3bf654b22ef502c877494f670d0a
Joshua <cutejosh2@gmail.com>
2022-09-29 00:18:22 +0100

    named exports

--- src/index.js
+++ src/index.js
@@ -2,38 +2,6 @@ import React from "react";
 import { createRoot } from 'react-dom/client'
 import './index.css';
 
-const books = [
-	{
-		id: 1,
-		img: "https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/81abeGoTMsL._AC_UL600_SR600,400_.jpg",
-		title: "Title one of books",
-		author: "author name one",
-	},
-	{
-		id: 2,
-		img: "https://images-na.ssl-images-amazon.com/images/I/81HE+JDes-L._AC_UL600_SR600,400_.jpg",
-		title: "Title two of booke",
-		author: "author name two",
-	},
-	{
-		id: 3,
-		img: "https://images-na.ssl-images-amazon.com/images/I/81HE+JDes-L._AC_UL600_SR600,400_.jpg",
-		title: "Title three of booke",
-		author: "author name three",
-	},
-	{
-		id: 4,
-		img: "https://images-na.ssl-images-amazon.com/images/I/81HE+JDes-L._AC_UL600_SR600,400_.jpg",
-		title: "Title four of booke",
-		author: "author name four",
-	},
-	{
-		id: 5,
-		img: "https://images-na.ssl-images-amazon.com/images/I/81HE+JDes-L._AC_UL600_SR600,400_.jpg",
-		title: "Title five of booke",
-		author: "author name five",
-	},
-]
 
 function BookList() {
 	return (
@@ -48,13 +16,11 @@ function BookList() {
 }
 
 const Book = (props) => {
-	// attribute, then event Handler
-	// onCLick and onMouseOver
 	const clickHandler = () => {
 		alert("Hello world");
 	}
 	return <article className="book" onMouseOver={()=>{
-		console.log("mouse is over the book")
+		console.log(props.title)
 	}}>
 		<img src={ props.img } alt="" />
 		<h2>{ props.title }</h2>
